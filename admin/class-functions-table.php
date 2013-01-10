RyanBayne/plugin-codex-generator

commit 2e60643cd3ad5482438ebeee1d1c6f848e636e31

Change the way hooks are parsed: use PCG_PHP_Reflect, extends PHP_Reflect. Tidy up admin tables & admin pages. Adds search support on hook admin tables. Massive code refactoring. (Sorry).

admin/class-functions-table.php
@@ -3,10 +3,10 @@
 if (!class_exists('WP_List_Table'))
 	require_once(ABSPATH . 'wp-admin/includes/class-wp-list-table.php');
 
-class Plugin_Codex_Generator_Functions_Table extends WP_List_Table {
+class Plugin_Codex_Generator_Functions_Table extends PCG_Admin_Table {
 
-	function __construct() {
-		parent::__construct(array( 'singular' => 'function', 'plural' => 'functions','ajax' => false));
+	function __construct( $args=array() ) {
+			parent::__construct(array( 'singular' => 'function', 'plural' => 'functions','ajax' => false));
     	}
 
 	function prepare_items() {
@@ -21,7 +21,7 @@ class Plugin_Codex_Generator_Functions_Table extends WP_List_Table {
 		$offset = $current_page > 1 ? $per_page*($current_page-1) : 0;
 
 		$query = array(
-			'type'=>'function',
+			'type'=>$this->_args['singular'],
 			'number' => $per_page,
 			'return' => 'array',
 			'offset' => $offset,
@@ -43,108 +43,6 @@ class Plugin_Codex_Generator_Functions_Table extends WP_List_Table {
             		'per_page'    => $per_page,
             		'total_pages' => ceil($total_items/$per_page),
         	) );
-	}
-
-
-	function display_tablenav( $which ) {
-
-		printf('<div class="tablenav %s">',esc_attr( $which ));
-		$this->extra_tablenav( $which );
-		$this->pagination( $which );
-		echo '<br class="clear" />';
-		echo '</div>';
-	}
-
-
-	function extra_tablenav($which) {
-
-		if( 'top' == $which ) {
-			$this->generate_docs();
-			$this->search_box( __('Name:','plugincodexgen'),'functions');
-			$this->path_dropdown();
-			submit_button( __( 'Filter','plugincodexgen' ), 'secondary', false, false, array( 'id' => 'function-query-submit' ) );
-			$this->reset_button();
-		}
-		else {
-			$this->reset_button();
-		}
-	}
-
-	function generate_docs(){
-		$options = array(
-			''=> __('Bulk Actions'),
-			'generate-documentation' => __('Generate/Update Documentation','plugincodexgen'),
-		);
-		echo '<select id="functon_action" name="plugincodexgen[action]">';
-		foreach( $options as $value => $label ) {
-			echo "<option value='{$value}'>{$label}</option>";
-		}
-		echo '</select>';
-		wp_nonce_field('plugincodexgen-bulk-action', '_pcgpnonce',false);
-		submit_button( 'Bulk Action', 'secondary', 'submit',false);
-	}
-
-
-	function search_box($text, $input_id) {
-
-		$input_id = $input_id . '-search-input';
-		?>
-		<label class="screen-reader-text" for="<?php echo $input_id ?>"><?php echo $text; ?>:</label>
-		<label for="<?php echo $input_id ?>"><span class="description"><?php echo $text; ?></span></label>
-		<input type="text" id="<?php echo $input_id ?>" name="s" value="<?php _admin_search_query(); ?>" />
-		<?php
-	}
-
-
-	function path_dropdown() {
-
-		$path = isset( $_GET['path'] ) ? esc_html($_GET['path']) : false;
-
-		?>
-	<label for="path"><span class="description"><?php _e('Path:','codex_gen'); ?></span></label>
-	<select id="path" name='path'>
-		<option value=""></option>
-		<?php
-
-		$tree = $this->explode_paths_to_tree($this->paths);
-		echo $this->print_tree($tree, $path);
-
-		?>
-	</select>
-		<?php
-	}
-
-
-	function explode_paths_to_tree($paths) {
-
-		$tree = array();
-		foreach( $paths as $path ) {
-			$parts = explode('/', $path);
-			$target =& $tree;
-			foreach( $parts as $part )
-				$target =& $target[$part];
-		}
-		return $tree;
-	}
-
-
-	function print_tree($tree, $current, $prepend = '', $val = '') {
-
-		foreach( $tree as $key => $leaf ) {
-
-			$value = trim( "{$val}/{$key}", '/' );
-			$selected = selected($value, $current, false);
-			echo "<option value='{$value}' {$selected} >{$prepend}{$key}</option>";
-
-			if( is_array($leaf) )
-				$this->print_tree($leaf, $current, '- '.$prepend, $val.'/'.$key);
-		}
-	}
-
-	function reset_button() {
-		$link = PLUGIN_CODEX_GENERATOR_LINK;
-		$anchor = __('Reset', 'codex_gen');
-		echo "<a href='{$link}' class='reset'>{$anchor}</a>";
 	}
 
 	function get_columns(){
@@ -195,26 +93,6 @@ class Plugin_Codex_Generator_Functions_Table extends WP_List_Table {
 		return esc_html( $description );
 	}
 
-
-	function column_arguments($item) {
-
-		$output = '';
-
-		foreach( $item->parameters as $param ) {
-
-			$type = isset( $param['type'] ) ? "<em>({$param['type']})</em>" : '';
-			$name = "<strong>\${$param['name']}</strong>";
-			$default = $param['has_default'] ? " = ".plugincodex_value_to_string($param['default']) : '';
-
-			$output .= "{$type} {$name} {$default}<br />";
-		}
-
-		$output = "<span class='code'>{$output}</span>";
-
-		return $output;
-	}
-
-
 	function column_return($item) {
 
 		if(!empty($item->doc['tags']['return'])) {
@@ -226,7 +104,6 @@ class Plugin_Codex_Generator_Functions_Table extends WP_List_Table {
 
 		return '';
 	}
-
 
 	function column_version($item) {
 
@@ -260,21 +137,6 @@ class Plugin_Codex_Generator_Functions_Table extends WP_List_Table {
 		return implode( ' / ', $links ).$line;
 	}
 
-	/*
-	* Checkbox column for Bulk Actions.
-	* 
-	* @see WP_List_Table::::single_row_columns()
-	* @param array $item A singular item (one full row's worth of data)
-	* @return string Text to be placed inside the column <td> (movie title only)
-	*/
-	function column_cb($item){
-        	return sprintf(
-	            '<input type="checkbox" name="%1$s[]" value="%2$s" />',
-        	    /*$1%s*/ 'function',  
-        	    /*$2%s*/ $item->name       //The value of the checkbox should be the record's id
-        	);
-    	}
-
 	function column_package( $item ){
 		$link = add_query_arg('package', $item->package, PLUGIN_CODEX_GENERATOR_LINK);
 		printf('<a href="%s" title="%s">%s</a>', 
@@ -282,30 +144,5 @@ class Plugin_Codex_Generator_Functions_Table extends WP_List_Table {
 			sprintf(esc_attr__('Filter by %s package','plugincodexgen'), $item->package), 
 			esc_html($item->package)
 		);
-	}
-
-	function column_get($item) {
-
-		$name = $item->name;
-		$href = admin_url('admin-ajax.php');
-
-		$query = array(
-			'action' => 'plugin_codex_gen_wiki',
-			'function' => esc_attr($name),
-			'width' => 800,
-			'height' => 500,
-		);
-
-		$href = esc_url(add_query_arg($query, $href));
-		$anchor = esc_html(__('Preview','plugincodexgen'));
-		$title = esc_attr(sprintf(__('Preview page markup for %s()','plugincodexgen'), $name));
-
-		$page ='';
-		if( $posts = get_posts(array('post_type'=>'pcg_function','post_status'=>'any','name'=>sanitize_title($item->name))) ){
-			$page = $posts[0];
-			$page = sprintf('<a target="_blank" href="%s"> Page </a> | ', get_permalink($page));
-		}
-
-		return $page."<a href='{$href}' class='thickbox' title='{$title}'>{$anchor}</a>";
 	}
 }

includes/class-pcg-php-reflect.php
@@ -1,0 +1,506 @@
+<?php
+
+class PCG_PHP_Reflect extends PHP_Reflect{
+
+	protected $page_package;
+
+
+	function __construct($args=array()) {
+		parent::__construct($args);
+		$this->parserToken['T_WPHOOK'] = array('PHP_Reflect_Token_WPHOOKS', array($this, 'parseToken'));
+    	}
+
+    /**
+     * Main Parser
+     *
+     * @return void
+     */
+    protected function parse()
+    {
+        $namespace        = FALSE;
+        $namespaceEndLine = FALSE;
+        $class            = FALSE;
+        $classEndLine     = FALSE;
+        $interface        = FALSE;
+        $interfaceEndLine = FALSE;
+        $trait            = FALSE;
+        $traitEndLine     = FALSE;
+
+        foreach ($this->tokens as $id => $token) {
+
+            if ('T_HALT_COMPILER' == $token[0]) {
+                break;
+            }
+
+            $tokenName  = $token[0];
+            $text       = $token[1];
+            $line       = $token[2];
+
+            $context = array(
+                'namespace' => $namespace,
+                'class'     => $class,
+                'interface' => $interface,
+                'trait'     => $trait,
+                'context'   => strtolower(str_replace('T_', '', $tokenName))
+            );
+
+
+		if( $context['context'] == 'string' && ( $text == 'do_action' || $text == 'apply_filters') ){
+			$context['context'] = 'wp-hook';
+			$tokenName = 'T_WPHOOK';
+		}
+
+            switch ($tokenName) {
+            case 'T_CLOSE_CURLY':
+                if ($namespaceEndLine !== FALSE
+                    && $namespaceEndLine == $line
+                ) {
+                    $namespace        = FALSE;
+                    $namespaceEndLine = FALSE;
+                }
+                if ($classEndLine !== FALSE
+                    && $classEndLine == $line
+                ) {
+                    $class        = FALSE;
+                    $classEndLine = FALSE;
+                }
+                if ($interfaceEndLine !== FALSE
+                    && $interfaceEndLine == $line
+                ) {
+                    $interface        = FALSE;
+                    $interfaceEndLine = FALSE;
+                }
+                if ($traitEndLine !== FALSE
+                    && $traitEndLine == $line
+                ) {
+                    $trait        = FALSE;
+                    $traitEndLine = FALSE;
+                }
+                break;
+            default:
+                if (isset($this->parserToken[$tokenName])) {
+                    $tokenClass = $this->parserToken[$tokenName][0];
+                    $token = new $tokenClass($text, $line, $id, $this->tokens);
+
+                    call_user_func_array(
+                        $this->parserToken[$tokenName][1],
+                        array(&$this, $context, $token)
+                    );
+                }
+                break;
+            }
+
+            if ($tokenName == 'T_NAMESPACE') {
+                $namespace        = $token->getName();
+                $namespaceEndLine = $token->getEndLine();
+
+            } elseif ($tokenName == 'T_USE') {
+                if ($class !== FALSE) {
+                    // warning: don't set $trait value 
+                    $traitEndLine = $token->getEndLine();
+                }
+
+            } elseif ($tokenName == 'T_TRAIT') {
+                $trait        = $token->getName();
+                $traitEndLine = $token->getEndLine();
+
+            } elseif ($tokenName == 'T_INTERFACE') {
+                $interface        = $token->getName();
+                $interfaceEndLine = $token->getEndLine();
+
+            } elseif ($tokenName == 'T_CLASS') {
+                $class        = $token->getName();
+                $classEndLine = $token->getEndLine();
+            }
+        }
+    }
+
+
+    function getFunctions()
+    {
+
+	$_functions = parent::getFunctions();
+
+	if( ! $_functions )
+		return array();
+
+	$this->page_package = $this->get_page_package();
+
+	//Get rid of namespacing
+	$_functions = array_pop($_functions);
+
+	$functions =array();
+	foreach( $_functions as $name => $_function ){
+
+		$function = new PCG_Function();
+		$function->name = $name;
+		$function->path = isset($_function['file']) ? plugincodex_sanitize_path($_function['file']): false;
+		$function->line = absint($_function['startLine']);
+
+		/* Parse the docblock (add links, etc) */
+		$function->doc = $this->parse_doc($_function['docblock']);
+		$function->long_desc = isset($function->doc['long_desc']) ? $function->doc['long_desc'] : '';
+		$function->short_desc = isset($function->doc['short_desc']) ? $function->doc['short_desc'] : '';
+
+		/* If the page docbloc has a package, assign it to function by default */
+		if( $this->page_package )
+			$function->package = $this->page_package;
+
+		/* Tags */
+		$tags = array('package','since','see','uses','used-by','link');
+		foreach( $tags as $tag ){
+			if( isset($function->doc['tags'][$tag] ) ){
+				$_tag = str_replace('-','_', $tag);
+				$function->{$_tag} = $this->parse_tag($tag, $function->doc['tags'][$tag] );
+			}
+		}
+		
+		/* Parse params from PHPReflect and merge with details from the docblock */
+		$function->parameters = $this->merge_params( $_function['arguments'], $function->doc['tags']['param']);		
+
+		/* Handle the @deprecated tag */
+		if( isset($function->doc['tags']['deprecated']) ){
+			$doc_tags = $function->doc['tags'];
+
+			//If function is superceded by a new one, this should be marked with @see
+			if( isset($doc_tags['see']) ){
+				$replacement = is_array($doc_tags['see']) ? $doc_tags['see'][0] : $doc_tags['see'];
+				$replacement = trim($replacement,'()');
+			}else{
+	                	$replacement = false;
+			}
+				       
+			//Note: $doc_tags['deprecated'] will be TRUE if @deprecated tag is present but has no value
+			$value = is_string($doc_tags['deprecated']) ? $doc_tags['deprecated'] : false;
+			list($version, $description) = plugincodex_explode(' ', $value, 2, false);
+			
+			if( $version )
+				$version = plugincodex_sanitize_version($version);
+
+			$function->deprecated = compact('version','description','replacement');
+		}
+
+		$functions[$name]  = $function;
+	}
+
+	return $functions;	
+     }
+
+
+     function getWphooks(){
+
+		$parsed_hooks = parent::getWphooks();
+		
+		if( !$parsed_hooks ) 
+			return array();
+
+		$parsed_hooks = array_pop($parsed_hooks);
+		$hooks = array();
+
+		foreach( $parsed_hooks as $name => $_hook ){
+
+			if( isset($hooks[$name]) ){
+				$hooks[$name]->location[] = array(
+						'path' => $this->filename,
+						'line' => $_hook['line'],
+				);
+	
+			}else{
+				$hook = new PCG_Hook();
+				$hook->name = $name;
+				$hook->type = $_hook['hooktype'];
+
+				$hook->doc =$this->parse_doc($_hook['docblock']);
+				$hook->long_desc = isset($hook->doc['long_desc']) ? $hook->doc['long_desc'] : '';
+				$hook->short_desc = isset($hook->doc['short_desc']) ? $hook->doc['short_desc'] : '';
+
+				foreach( $_hook['arguments'] as $arg => $arg_array ){
+					if( !isset($_hook['arguments'][$arg]['name'] ) ){
+						if( isset($arg_array['typeHint']) ){
+							$_hook['arguments'][$arg]['name'] = '$'.$arg_array['typeHint'].'_'.$arg;
+						}else{
+							$_hook['arguments'][$arg]['name'] = '$arg_'.$arg;
+						}
+					}
+				}
+
+				/* Parse params from PHPReflect and merge with details from the docblock */
+				$hook->parameters = $this->merge_params( $_hook['arguments'], $hook->doc['tags']['param']);		
+
+				$hook->path =$this->filename;
+				$hook->line = $_hook['line'];
+				$hook->location[] = array(
+					'path' => $this->filename,
+					'line' => $_hook['line'],
+				);
+
+				/* Tags */
+				$tags = array('package','since','see','uses','used-by','link');
+				foreach( $tags as $tag ){
+					if( isset($hook->doc['tags'][$tag] ) ){
+						$_tag = str_replace('-','_', $tag);
+						$hook->{$_tag} = $this->parse_tag($tag, $hook->doc['tags'][$tag] );
+					}
+				}
+		
+				$hooks[$name] = $hook;
+			}
+		}
+	
+		return $hooks;		
+    	}
+
+
+
+
+	function get_page_package(){
+		if( !isset($this->filename) )
+			return false;
+
+		//Hack to get page-level doc: reflect::tokens has been changed from protected to achieve this
+		$tokens = $this->tokens;
+
+		$docblocs = 0;
+		$docComment ='';
+		foreach( $tokens as $token ){
+
+			if( 'T_DOC_COMMENT' == $token[0] ){
+				if( 0 == $docblocs )
+					$docComment = $token[1];
+	
+			$docblocs++;
+		}	
+		
+		if( 'T_FUNCTION' == $token[0] )
+			break;
+		}
+
+		if( $docblocs > 1 ){
+			$result = array('fullPackage' => '','package'     => '','subpackage'  => '');
+			if (preg_match('/@package[\s]+([\.\-\w]+)/', $docComment, $matches)) {
+				$result['package']     = $matches[1];
+				$result['fullPackage'] = $matches[1];
+	        	}
+
+			if (preg_match('/@subpackage[\s]+([\.\-\w]+)/', $docComment, $matches)) {
+				$result['subpackage']   = $matches[1];
+				$result['fullPackage'] .= '.' . $matches[1];
+	       		}
+			return $result['package'];		
+		}
+		return false;
+	}
+
+
+	
+	function parse_tag( $tag, $value ){
+		if( !is_array($value) )
+			$value = array($value);
+
+		switch( $tag ):
+			case 'link':
+				foreach( $value as $i => $link ){
+					$link = explode(' ', trim($link));
+					$url = array_shift($link);
+					$description = implode(' ',$link);
+					if( empty($description ) )
+						$description = $url;
+					$value[$i] = compact('url','description');
+				}
+			break;
+
+			case 'package':
+				if( empty($value) && $this->page_package )
+					$value = $this->page_package;
+			break;
+
+			case 'since':
+				$value = plugincodex_sanitize_version($value[0]);
+			break;
+
+		endswitch;
+
+		return $value;
+	}
+
+
+	/**
+	 * Parses PHPDoc
+	 *
+	 * @param string $doc PHPDoc string
+	 * @return array of parsed information
+	 */
+	function parse_doc($doc) {
+
+		$short_desc = $long_desc = $last_tag = '';
+		$tags = array('param'=>array());
+
+		$did_short_desc = false;
+		$did_long_desc = false;
+		$prepend_short_desc = '';
+		$prepend_long_desc = '';
+
+		$doc = explode("\n", $doc);
+
+		foreach( $doc as $line ) {
+
+			$line = trim($line);
+			$line = trim($line, " \t");
+			//Don't use ltrim - Only want asterix and the following space removed
+			$line = plugincodex_remove_from_start( $line, '*');
+			$line = plugincodex_remove_from_start( $line, ' ');
+			$trimmed_line = trim($line,' ');
+
+			// Start or end
+			if ('/' == trim($trimmed_line,'*'))
+				continue;
+
+			// Empty lines as start
+			if (empty($line) && !$short_desc)
+				continue;
+
+			// Tag, also means done with descriptions
+			if( '@' == substr($trimmed_line, 0, 1) ) {
+
+				if( !$did_long_desc )
+					$did_long_desc = $did_short_desc = true;
+
+				$line = trim($trimmed_line, '@');
+				list($tag, $value) = plugincodex_explode(' ', $line, 2, true);
+				$last_tag = $tag;
+
+				if (!isset($tags[$tag]))
+					$tags[$tag] = $value;
+				elseif (!is_array($tags[$tag]))
+					$tags[$tag] = array($tags[$tag], $value);
+				else
+					$tags[$tag][] = $value;
+
+				continue;
+			}
+
+			// Short description
+			if( !$did_short_desc ) {
+
+				if( empty($line) ) {
+					$did_short_desc = true;
+
+				}else {
+					$short_desc .= $prepend_short_desc ."\n".$line;
+					$prepend_short_desc = ' ';
+				}
+
+				continue;
+			}
+
+			// Long description
+			if( !$did_long_desc ) {
+
+				if( !empty($line) ) {
+					$long_desc .= $prepend_long_desc ."\n". $line;
+					$prepend_long_desc = ' ';
+
+				}else {
+					 $prepend_long_desc = "\n";
+				 }
+
+				continue;
+			}
+
+			// Additional line for a tag
+			if( !empty($line) && !empty($last_tag)  ) {
+
+				if( is_array($tags[$last_tag]) ) {
+					end( $tags[$last_tag] );
+					$key = key( $tags[$last_tag] );
+					$tags[$last_tag][$key] .= ' ' . $line;
+
+				}else {
+					$tags[$last_tag] .= ' ' . $line;
+				}
+
+				continue;
+			}
+		}
+		
+		/* Replace inline {@link} */
+		$long_desc = preg_replace_callback("/{@link ([^}]*)}/i", array(__CLASS__,'inline_make_clickable'), $long_desc);	
+		$short_desc = preg_replace_callback("/{@link ([^}]*)}/i", array(__CLASS__,'inline_make_clickable'), $short_desc);		
+
+		/* Replace inline {@see} */
+		$long_desc = preg_replace_callback("/{@see ([^}]*)}/i",array(__CLASS__,'add_see_link'), $long_desc);
+		$short_desc = preg_replace_callback("/{@see ([^}]*)}/i",array(__CLASS__,'add_see_link'),$short_desc);
+
+		return compact( 'short_desc', 'long_desc', 'tags' );
+	}
+
+
+	function add_see_link( $replace ){
+
+		$reference = trim($replace[1], '`');
+		$link = plugincodex_find_reference_link($reference);
+		if( $link )
+			$replace[1] = sprintf('<a href="%s">%s</a>', $link, $replace[1]);
+
+		return $replace[1];
+	}
+
+
+	function inline_make_clickable( $replace ){
+		$replace = explode(' ', $replace[1]);
+		$url = array_shift($replace);
+		$description = implode(' ', $replace);
+
+		if( !$description )
+			$description = $url;
+
+		return sprintf('<a href="%s">%s</a>', $url, $description);
+	}
+
+
+	/**
+	 * Merges parameter information, obtained from PHP_Reflect and PHPDoc.
+	 *
+	 * @param array $from_params info from PHP_Reflect
+	 * @param array $from_tags info from PHPDoc
+	 * @return array merged info
+	 */
+	 function merge_params( $from_params, $from_tags ) {
+
+		$params = array();
+
+		foreach ($from_params as $param) {
+			$name = trim($param['name'], '$');
+
+			$append = array(
+				'name' => $name,
+				'has_default' => isset($param['defaultValue']),
+				'optional' =>  isset($param['defaultValue']) ? 'optional' : 'required',
+			);
+
+			if( $append['has_default'] )
+				$append['default'] = $param['defaultValue'];
+
+			$params[$name] = $append;
+		}
+
+		foreach( $from_tags as $param ){
+			
+			list($type, $name, $description) = plugincodex_explode(' ', $param, 3, '');
+			$name = trim($name, '$');
+
+			if( !isset($params[$name]) )
+				continue;
+
+			if ( !empty($type) )
+				$params[$name]['type'] = $type;
+
+			if ( !empty($description) )
+				$params[$name]['description'] = $description;
+		}
+
+		return $params;
+	}
+}
+
+?>

commit 9981bd8aad11216669fed71114e9287ba53d5045

Fixes bug introduced in 2e60643cd3ad

admin/class-functions-table.php
@@ -21,7 +21,6 @@ class Plugin_Codex_Generator_Functions_Table extends PCG_Admin_Table {
 		$offset = $current_page > 1 ? $per_page*($current_page-1) : 0;
 
 		$query = array(
-			'type'=>$this->_args['singular'],
 			'number' => $per_page,
 			'return' => 'array',
 			'offset' => $offset,

includes/class-pcg-php-reflect.php
@@ -6,6 +6,13 @@ class PCG_PHP_Reflect extends PHP_Reflect{
 
 
 	function __construct($args=array()) {
+
+		$args = array_merge($args, 
+				array(
+		  		'containers'=>array('wp-hook' => 'wphooks'),
+				'properties'=>array('wp-hook'=>array('line', 'docblock', 'arguments', 'hooktype'))
+			));
+		
 		parent::__construct($args);
 		$this->parserToken['T_WPHOOK'] = array('PHP_Reflect_Token_WPHOOKS', array($this, 'parseToken'));
     	}

includes/class-pcg-query.php
@@ -79,7 +79,7 @@ class PCG_Query {
 			return false;
 		}
 
-		if( !empty($this->args['type']) && $object->type !=$object['type'] ){
+		if( !empty($this->args['type']) && $object->type !=$this->args['type'] ){
 			return false;
 		}
 
@@ -331,10 +331,7 @@ class PCG_Hook_Query extends PCG_Query{
 
 		foreach( $files as $file ){
 
-			$reflect = new PCG_PHP_Reflect(array(
-		  		'containers'=>array('wp-hook' => 'wphooks'),
-				'properties'=>array('wp-hook'=>array('line', 'docblock', 'arguments', 'hooktype'))
-			));
+			$reflect = new PCG_PHP_Reflect();
 			$reflect->scan($file);
 			$parsed_hooks = $reflect->getWphooks();
 			if( !$parsed_hooks ) 